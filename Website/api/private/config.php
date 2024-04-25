d008b2bbb31ca3e6a5b5421b9746e33a0d4109dc
danil2201 <125144087+danil2201@users.noreply.github.com>
2024-04-25 10:44:16 +0300

    Update config.php

--- Website/api/private/config.php
+++ Website/api/private/config.php
@@ -1,28 +1,48 @@
 <?php
+
 ini_set('display_errors', 1);
+
 ini_set('display_startup_errors', 1);
+
 error_reporting(E_ALL);
 
+
 define("SITE_CONFIG", 
+
 	[
+ 
 		"database" =>
+  
 		[
-			"host" => "127.0.0.1",
-			"schema" => "polygon",
-			"username" => "root",
-			"password" => ""
+  
+			"host" => "127.0.0.1",  // Change to the IP address of your SQL database
+   
+			"schema" => "polygon", // Do not change the db name!
+   
+			"username" => "root", // Username
+   
+			"password" => "" // Password
+   
 		],
 
 		"site" =>
+  
 		[
-			"name" => "Project Polygon",
-			"name_secondary" => "Polygon",
-			"currencyName" => "Pizzas"
+			"name" => "Project Polygon", // Site name
+   
+			"name_secondary" => "Polygon", // Secondary name
+   
+			"currencyName" => "Pizzas" // Currency
+   
 		],
 
 		"captcha" =>
+  
 		[
 			"siteKey" => "undefined",
+   
 			"privateKey" => "undefined"
+   
 		]
-	]);
+  
+]);
